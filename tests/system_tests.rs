59ea6ed510e89921786f61c4649140691e6127fe
Victor Palade <victor@cloudflavor.io>
2024-07-02 22:44:27 +0200

    wasi: load wasi compiled plugins.
    Get rid of the plugins json spec and move to loading WASI compiled modules.
    The validation is done by calling the default exported function now.
    The plugin spec resides in cloudflavor/skyforge.
    
    Signed-off-by: Victor Palade <victor@cloudflavor.io>

--- tests/system_tests.rs
+++ tests/system_tests.rs
@@ -1,15 +1,6 @@
 use anyhow::Ok;
-use once_cell::sync::OnceCell;
-
-static INIT: OnceCell<()> = OnceCell::new();
 
 fn setup() {}
 
 #[test]
-fn system_test_load_plugins() {
-    INIT.get_or_try_init(|| {
-        setup();
-        Ok(())
-    })
-    .unwrap();
-}
+fn system_test_load_plugins() {}
